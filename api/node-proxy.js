fe5129bb3ccf00bad66121996f7b2f0321539b28
Jason.lu <vrcms@users.noreply.github.com>
2021-07-09 10:08:42 +0800

    Update node-proxy.js

--- api/node-proxy.js
+++ api/node-proxy.js
@@ -1,16 +1,15 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 const auth = require('basic-auth');
-
 var Cookies = require('cookies');
 //cookies进行签名(加密)
 var keys = ['keyboard cat'];
 
 // Create a proxy server with custom application logic
 const proxy = httpProxy.createProxyServer({changeOrigin: true, autoRewrite: true, hostRewrite: true, followRedirects: true});
-const envORIGIN = process.env.ORIGIN;
-var origin = (envORIGIN && envORIGIN != '') ? envORIGIN:'https://www.google.com';//默认值
-var cacheorigin;
+const envORIGIN = process.env.ORIGIN?process.env.ORIGIN:'https://www.google.com';//默认值
+var origin = envORIGIN;//默认值
+
 
 
 const server = http.createServer(function(req, res) {
@@ -21,7 +20,7 @@ const server = http.createServer(function(req, res) {
 //   const username = process.env.USERNAME;
   
   const password = '123456';//默认密码
-  var username = 'admin';//用户名是网址
+  const username = 'admin';//用户名是网址
   
   
   const credentials = auth(req);
@@ -53,33 +52,31 @@ const server = http.createServer(function(req, res) {
   var cookies = new Cookies(req, res, { keys: keys });
   if(req && req.url.substring(0,3).toUpperCase() == '/F/'){
     //更改目标
-     var targeturl = 'https://www.google.com';//默认url
+     var targeturl = envORIGIN;//默认url
      var inurl = req.url.substring(3);
-     console.log('目标:',inurl);
+     
     if(inurl.substring(0,4).toUpperCase() == 'HTTP' ) {      
       //把丢失的/找回来
       inurl = inurl.replace('https:/','https://');
       inurl = inurl.replace('http:/','http://');
       targeturl = inurl;
-      console.log('开始变:',targeturl);
+      
     }
      cookies.set('lastorigin', targeturl, { signed: true,maxAge:0 }); //永久有效 
      res.statusCode = 200;
-      //res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+      
       res.end('<!DOCTYPE html><html><head><script language="javascript" type="text/javascript">window.location.href="/";</script></head>cookie changed!</html>');
   }
   
   if(req && req.url.substring(0,3).toUpperCase() == '/C/'){
       origin = envORIGIN;//默认值  
       cookies.set('lastorigin', '', { signed: true,maxAge:0 }); //删除 
-      res.statusCode = 200;
-      //res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+      res.statusCode = 200;      
       res.end('<!DOCTYPE html><html><head><script language="javascript" type="text/javascript">window.location.href="/";</script></head>cookie clean!</html>');
   }
   
   var lastorigin = cookies.get('lastorigin', { signed: true });
-  console.log('65 lastcookie==',lastorigin);
-  
+    
   if(lastorigin && (lastorigin.indexOf('://') != -1)){
     origin = lastorigin
   } 
@@ -98,6 +95,6 @@ server.listen(port);
 
 
 const isAuthed = function (credentials, username, password) {
-    //return credentials.name === username && credentials.pass === password;
-  return credentials.pass === password;
+    return credentials.name === username && credentials.pass === password;
+  
 }
